ee100bf7107a17ff7dd53905dcd778d3b5e146dc
BradBolander <bolanderbrad@gmail.com>
2018-01-03 17:08:40 -0600

    added google maps api reference, built out initial map and marker adding logic.

---
+++ public/javascripts/app.js
@@ -0,0 +1,22 @@
+
+
+var App = React.createClass({
+    
+    componentDidMount: function() {
+      
+    },  
+
+    render: function() {
+      return (
+        <div id="app">
+           <h1 className="site-header">Metorite Landing Locations</h1>
+        </div>
+      );
+    }
+  });
+
+
+  ReactDOM.render(
+    <App/>,
+    document.getElementById('container')
+  );
