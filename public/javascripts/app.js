1e9e389828a77c328b50459464c7dc38f4eac03b
BradBolander <bolanderbrad@gmail.com>
2018-01-08 16:57:20 -0600

    moved map building logic into componentDidMount

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,7 +3,32 @@
 var App = React.createClass({
     
     componentDidMount: function() {
-      
+      var map;
+      $.ajax({
+          method: "GET",
+          url: "https://data.nasa.gov/resource/gh4g-9sfh.json",
+          dataType: "json",
+          success: function(data) {
+            initMap(data);
+          }
+      });
+  
+      function initMap(data) {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 0, lng: 0},
+          zoom: 3
+        });
+  
+        for (i = 0; i < data.length; i++) { 
+          if (data[i].geolocation) {
+            var uluru = {lat: Number(data[i].geolocation.latitude), lng: Number(data[i].geolocation.longitude)};
+            var marker = new google.maps.Marker({
+              position: uluru,
+              map: map
+            });
+          }
+        }
+      }
     },  
 
     render: function() {
